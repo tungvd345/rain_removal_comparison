49595b5b9cad13f4e1b658cb4bde48cc51e91cf3
tungvd345 <vdtung1995@gmail.com>
2021-02-19 17:24:11 +0900

    DGNLNet - env: pytorch_120

---
+++ 16.DGNL-Net/config.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import os
+
+root_raincityscapes = '/home/kaist/rain_datasets/'
+train_raincityscapes_path = root_raincityscapes + 'train_301_350/'
+#os.path.join(root_raincityscapes, 'train')
+#train_raincityscapes_gt_path = os.path.join(root_raincityscapes, 'image')
+
+test_raincityscapes_path = root_raincityscapes + 'test_with_train_param_v5/'
+#os.path.join(root_raincityscapes, 'test')
+#root_raincityscapes
+#os.path.join(root_raincityscapes, 'test')
+#test_raincityscapes_gt_path = os.path.join(root_raincityscapes, 'image')
+
---
+++ 16.DGNL-Net/infer.py
@@ -0,0 +1,100 @@
+import os
+import time
+
+import torch
+from PIL import Image
+from torch.autograd import Variable
+from torchvision import transforms
+
+from nets import basic, depth_predciton, basic_NL, DGNLNet
+from config import test_raincityscapes_path
+from misc import check_mkdir
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+torch.manual_seed(2019)
+torch.cuda.set_device(0)
+
+ckpt_path = './ckpt'
+exp_name = 'DGNLNet'
+args = {
+    'snapshot': '40000_heavyrain',
+    'depth_snapshot': ''
+}
+
+transform = transforms.Compose([
+    transforms.Resize([512,1024]),
+    transforms.ToTensor() ])
+
+#root = os.path.join(test_raincityscapes_path, 'in')
+
+root = 'C:/DATASETS/Heavy_rain_image_cvpr2019/test_with_train_param_v5/in/'
+
+to_pil = transforms.ToPILImage()
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def main():
+    net = DGNLNet().cuda()
+
+    if len(args['snapshot']) > 0:
+        print('load snapshot \'%s\' for testing' % args['snapshot'])
+        net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth'),
+                                       map_location=lambda storage, loc: storage.cuda(0)))
+
+    net.eval()
+    # print('num of param 1:', count_parameters(net))
+    avg_time = 0
+    total_time = 0
+
+    with torch.no_grad():
+
+        img_list = [img_name for img_name in os.listdir(root)]
+        start = time.time()
+
+        for idx, img_name in enumerate(img_list):
+            check_mkdir(
+                os.path.join(ckpt_path, exp_name, '(%s) prediction_%s' % (exp_name, args['snapshot'])))
+            if len(args['depth_snapshot']) > 0:
+                check_mkdir(
+                    os.path.join(ckpt_path, exp_name, '(%s) prediction_%s' % (exp_name, args['depth_snapshot'])))
+
+            img = Image.open(os.path.join(root, img_name)).convert('RGB')
+            w, h = img.size
+            img_var = Variable(transform(img).unsqueeze(0)).cuda()
+
+            start_time = time.time()
+
+            #res, dps = net(img_var)
+            res = net(img_var)
+            # print('num of param 2:', count_parameters(net))
+
+            torch.cuda.synchronize()
+
+            # if len(args['depth_snapshot']) > 0:
+            #     depth_res = depth_net(res, depth_optimize = True)
+
+            avg_time = avg_time + time.time() - start_time
+
+            print('predicting: %d / %d, avg_time: %.5f' % (idx + 1, len(img_list), avg_time/(idx+1)))
+
+            result = transforms.Resize((h, w))(to_pil(res.data.squeeze(0).cpu()))
+
+
+            result.save(
+                os.path.join(ckpt_path, exp_name, '(%s) prediction_%s' % (
+                    exp_name, args['snapshot']), img_name))
+
+    total_time = time.time() - start
+    print('total time: ', total_time)
+    
+            # if len(args['depth_snapshot']) > 0:
+            #     depth_result = transforms.Resize((h, w))(to_pil(dps.data.squeeze(0).cpu()))
+            #     depth_result.save(
+            #         os.path.join(ckpt_path, exp_name, '(%s) prediction_%s' % (
+            #             exp_name, args['depth_snapshot']), img_name))
+
+
+if __name__ == '__main__':
+    main()
